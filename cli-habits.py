be56d06afdfbd1fc9efe14fe0fbfe1d5308fe38c
jckstllrd <jackstallard17@gmail.com>
2025-12-11 08:53:42 +0000

    Add the 'done' argument for marking a habit as complete

--- cli-habits.py
+++ cli-habits.py
@@ -3,6 +3,9 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('add', nargs=2, help='This argument allows the user to add a new habit to their tracker.')
+parser.add_argument('done', nargs=2, help='This argument allows the user to mark the exact time and date that they completed the habit')
+
 
 args = parser.parse_args()
+print(args)
 print(args.add)
