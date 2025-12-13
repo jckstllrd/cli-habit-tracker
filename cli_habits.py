55d68451eece4942130314682a5d97ab2a22061d
jckstllrd <jackstallard17@gmail.com>
2025-12-13 15:11:38 +0000

    Add status arg, remove dummy test code

--- cli_habits.py
+++ cli_habits.py
@@ -6,14 +6,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-a','--add',nargs=1, help='This argument allows the user to add a new habit to their tracker.')
 parser.add_argument('-d','--done', nargs=1, help='This argument allows the user to mark the exact time and date that they completed the habit')
-
+parser.add_argument('-s','--status', help='This argument outputs the list of current habits/streaks')
 
 args = parser.parse_args()
 print(args.add)
 print(args.add[0])
-
-data_test = {
-    args.add[0]: "today's date"
-}
-
-jw.add_habit(data_test)
