6a46d6d4966cb879d1058c57a75ddff9aa33bf24
jckstllrd <jackstallard17@gmail.com>
2025-12-15 11:11:53 +0000

    Create habit module for json templates

---
+++ habits.py
@@ -0,0 +1,15 @@
+def create_habits():
+    habits = {
+        'habits': [
+            
+        ]
+    }
+    return habits
+    
+def add_habit(name):
+    habit = {
+        'name': f'{name}',
+        'dates_completed': [],
+        }
+    return habit
+    
