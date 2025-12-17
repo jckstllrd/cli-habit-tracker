f8703ebec0742698562e6c7d76faaf779ab09dfd
jckstllrd <jackstallard17@gmail.com>
2025-12-17 10:15:36 +0000

    Add initialise_habits function for creating a blank file if none exists

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,5 +1,8 @@
 import argparse
+import json
 import os
+
+import habits
 
 
 
@@ -13,20 +16,33 @@ def setup_args():
     args = parser.parse_args()
     return args
 
-# def initialise_habits()
+def initialise_habits():
+    file_path = 'data/habits.json'
+    data_template = {
+        
+    }
+    with open(file_path, 'w') as f:
+        habits_dict = json.dump(data_template, f)
+    return habits_dict
 
 def load_habits():
     file_path = 'data/habits.json'
+    habits_dict = {}
     try: 
         file_size = os.path.getsize(file_path)
-
         if file_size == 0:
             print("File is empty")
+            # initialise new habits file
+            habits_dict = initialise_habits()
         else:
             print("File is NOT empty")
+            # just load in the file
+            habits_dict = json.dump(file_path)
     except FileNotFoundError as e:
         print("File NOT found")
-    return 
+        # initialise new habits file
+        habits_dict = initialise_habits()
+    return habits_dict
 
 if __name__ == '__main__':
     args = setup_args()
